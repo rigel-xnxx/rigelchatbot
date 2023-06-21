f4e252f9373df3ad7a47110f37477f0d5f8444c8
Rigel <paroxsymalrage@gmail.com>
2023-06-20 23:53:20 -0300

    updated server url

--- client/script.js
+++ client/script.js
@@ -102,7 +102,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from the server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://rigel-chatbot.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
